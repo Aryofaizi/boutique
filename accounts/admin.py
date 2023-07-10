422916dac0aa13a65ac249f7995374347a79aa81
aryo faizi <aryofaizi@gmail.com>
2023-07-10 21:20:51 +0330

    CustomUserAdmin and the CustomUserCreationForm and CustomUserChangeForm created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = UserAdmin.list_display + ("phone_number", )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone_number",)}),
+    )
+    add_fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone_number",)}),
+    )
+
+
