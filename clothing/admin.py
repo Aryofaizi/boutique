ee05579e5aa8909f3f0717576fdd725677551a16
aryo faizi <aryofaizi@gmail.com>
2023-07-14 18:01:33 +0330

    ClothAdmin initlized for Cloth and ClothType models

--- clothing/admin.py
+++ clothing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Clothes
 
-# Register your models here.
+
+@admin.register(Clothes)
+class ClothAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "cloth_type")
--- clothing/models.py
+++ clothing/models.py
@@ -1,2 +1,21 @@
 from django.db import models
-# Create your models here.
+from accounts.models import CustomUser
+
+
+class ClothType(models.Model):
+    choices = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+
+class Clothes(models.Model):
+    name = models.CharField(max_length=100)
+    cloth_type = models.ForeignKey(ClothType, on_delete=models.CASCADE)
+    cloth_width = models.IntegerField(null=True, blank=True)
+    cloth_length = models.IntegerField(null=True, blank=True)
+    cloth_color = models.ForeignKey(ClothType, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    designers = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}: {self.cloth_type}"
