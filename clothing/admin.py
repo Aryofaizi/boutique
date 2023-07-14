5544e37c91c26a23deb4b69b7f80f1d4292158d2
aryo faizi <aryofaizi@gmail.com>
2023-07-14 18:08:49 +0330

    ClothTypeAdmin initialized

--- clothing/admin.py
+++ clothing/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Clothes
+from .models import Clothes, ClothType
 
 
 @admin.register(Clothes)
 class ClothAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "cloth_type")
+
+
+@admin.register(ClothType)
+class ClothTypeAdmin(admin.ModelAdmin):
+    list_display = ("choices", "color",)
